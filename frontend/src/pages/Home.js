87dfb154ef8b874e4075d341ef95e343a4a2457e
Mincy <23011625@myrp.edu.sg>
2025-01-17 21:16:34 +0800

    im tired

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,21 +7,37 @@ import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
     const {workouts, dispatch} = useWorkoutsContext()
+    const [searchTerm, setSearchTerm] = useState('')
+
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts`);
-            const json = await response.json()
+            try {
+                const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts`);
+                const data = await response.json();
 
-            if (response.ok) {
-               dispatch({type: 'SET_WORKOUTS', payload: json})
-        }
+                if (response.ok) {
+                    dispatch({ type: 'SET_WORKOUTS', payload: data });
+                }
+            } catch (error) {
+                console.error('Failed to fetch workouts:', error);
+            }
         }
         fetchWorkouts()
     }, [dispatch])
 
+    const filteredWorkouts = workouts.filter((workout) => {
+        return workout.title.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+
     return (
         <div className="home">
+            <input
+            type = "text"
+            placeholder="Search for a workout"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <div className="workouts">
                 {workouts && workouts.map((workout) => (
                     <WorkoutDetails key={workout._id} workout={workout}/>
